317749edcf94e1d34c435d936cdc1d74a9cfb260
Phoenix210 <Phoenix210@gmail.com>
2025-04-29 10:50:57 +0200

    Dreieck Spieler zu sehen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -8,15 +9,22 @@ def main():
     print("Screen height:", SCREEN_HEIGHT)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    Fpsclock = pygame.time.Clock()
+    dt = 0
+    The_player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
+# Game Loop
     infinite = 0
     while infinite == 0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = Fpsclock.tick(60) / 1000
+        
+        
         pygame.Surface.fill(screen, ("black"))
+        The_player.draw(screen)
         pygame.display.flip()
-
-
 
 
 
